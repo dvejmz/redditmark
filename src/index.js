3af3d64d088d8221d40b67901b0418c5d32dbc01
dvejmz <dvejmz@gmail.com>
2019-02-16 02:11:34 +0000

    Install Material UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CookiesProvider } from 'react-cookie';
+import 'typeface-roboto';
 import App from './App';
 import createReddit from './api/reddit';
 import createRequest from './api/request';
