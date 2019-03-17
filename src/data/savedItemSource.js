535b71c9180493d905aa898b14ba7b3e7f9f5d17
dvejmz <dvejmz@gmail.com>
2019-03-17 23:07:03 +0000

    Pass UID to /saved API endpoint

--- src/data/savedItemSource.js
+++ src/data/savedItemSource.js
@@ -1,10 +1,13 @@
+const md5 = require('md5');
+
 export default function (request, authToken) {
     async function getSavedItems() {
+        const tokenHash = md5(authToken);
         let body = null;
         let status = null;
         try {
             const response = await request.get(
-                `http://localhost:3002/saved`,
+                `http://localhost:3002/saved?uid=${tokenHash}`,
                 {
                     'Authorization': `Bearer ${authToken}`,
                 },
