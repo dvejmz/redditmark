7455c116b84d5851b472b63ccc316b38d552bfbb
dvejmz <dvejmz@gmail.com>
2021-01-02 22:38:58 +0000

    Add container styles to Saved page

--- packages/ui/src/components/Saved.jsx
+++ packages/ui/src/components/Saved.jsx
@@ -3,24 +3,60 @@ import React, { useState } from 'react';
 import {
    useQuery,
  } from 'react-query';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import { Save } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles';
+
 
 import SavedList from '../components/SavedList';
 import Error from '../components/Error';
 
+const styles = {
+    root: {
+        width: '100%',
+        maxWidth: 800,
+        margin: 'auto',
+    },
+    headingbar: {
+        padding: '10px 10px 0 20px',
+    },
+    searchfield: {
+        padding: '5px',
+    },
+    toggleGroup: {
+        width: '167px',
+        fontSize: 'smaller',
+    },
+};
+
 const Saved = ({
+    classes,
     getAccessToken,
     fetchSavedItems,
 }) => {
     const tokenResult = useQuery('token', getAccessToken);
     const token = tokenResult.data;
-    const { isLoading, isIdle, isError, error, data: allItems } = useQuery('savedItems', () => fetchSavedItems(token), { enabled: !!token });
+    const {
+        isLoading,
+        isIdle,
+        isError,
+        error,
+        data: allItems
+    } = useQuery('savedItems', () => fetchSavedItems(token), { enabled: !!token });
 
     if (isError) {
         return (<div><Error message={error.message} /></div>);
     }
 
     return (
-        <div>
+        <div id="saved" className={classes.root}>
+            <Paper>
             {isIdle || isLoading ?
                 'Loading...'
                 : (
@@ -29,8 +65,9 @@ const Saved = ({
                     </div>
                 )
             }
+            </Paper>
         </div>
     );
 };
 
-export default Saved;
+export default withStyles(styles)(Saved);
